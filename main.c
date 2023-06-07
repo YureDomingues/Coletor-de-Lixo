37908de806976cad0ca2daff268747c64d1573c9
jg-moren <jgmoren@gmail.com>
2023-06-07 19:49:26 -0300

    criacao da biblioteca estatica

--- main.c
+++ main.c
@@ -3,14 +3,17 @@
 #include <stdlib.h>
 
 int main(){
-    int *a = (int*) malloc2(sizeof(int));
-    int *b;
-    while(1){
-        b = malloc2(sizeof(int));
-        atrib2(&b, a);
-        print_lixo();
-        dump();
-    }
-
+    int *v = malloc2(sizeof(int));
+    *v = 10;
+    int *w = malloc2(sizeof(int));
+    dump();
+    *w = 20;
+    atrib2(&v,w);
+    dump();
+    char *c = "Z";
+    dump();
+    atrib2(&w,NULL);
+    dump();
+    print_lixo();
     return 0;
 };
